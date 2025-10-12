d042b85cff24d2b696094a602775db1ca1812371
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2025-10-13 07:57:37 +1000

    Update sketch.js

--- sketches/epicycles-collection/icosahedron/sketch.js
+++ sketches/epicycles-collection/icosahedron/sketch.js
@@ -2,148 +2,143 @@
  Title: Icosahedron with lower frequencies
  Author: Juan Carlos Ponce Campuzano
  Website: https://jcponce.github.io/
- Date: 12-Nov-2025
+ Date: 12-Oct-2025
  License: Creative Commons Attribution-NonCommercial 4.0 International License
  http://creativecommons.org/licenses/by-nc/4.0/
- 
- Comments: Finally I was able to make a simplified 
- version of the Discrete Fourier Transform (DFT), 
- now with lower frequencies so you can get a 
- better approximation of the signal. It has 
- also the approximation curve.
 
 */
 
+//===========================
+// Globals
+//===========================
 let x = [];
 let fourierX;
-let time;
+let time = 0;
 let path = [];
-
 let sliderTerms;
 let sel;
-let show = true;
+let showEpicycles = true;
 let scl;
 
+//===========================
+// p5.js Setup
+//===========================
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(47);
 
-  // Compute fourier coefficients with DFT
+  setupScale();
+  computeFourier();
+
+  setupUI();
+  // console.log('Fourier coefficients:', fourierX.length);
+}
+
+//===========================
+// p5.js Draw
+//===========================
+function draw() {
+  background(255);
+  translate(width / 2 - 70, height / 2);
+
+  if (showEpicycles) {
+    drawEpicycles();
+  } else {
+    drawApproximation();
+  }
+
+  // Update time
+  time += 0.006; // or use TWO_PI / fourierX.length
+}
+
+//===========================
+// Setup Helpers
+//===========================
+function setupScale() {
+  scl = (windowWidth < 1400 ? 0.05 : 0.04) * width;
+}
+
+function computeFourier() {
   const skip = 1;
   const size = 1;
-  if (windowWidth < 1400) {
-    scl = 0.05 * width;
-  } else {
-    scl = 0.04 * width;
-  }
+
+  x = [];
   for (let i = 0; i < densified.length; i += skip) {
-    const c = new Complex(scl * (densified[i].x * size), -scl * (densified[i].y * size));
-    x.push(c);
+    x.push(new Complex(scl * densified[i].x * size, -scl * densified[i].y * size));
   }
 
-  if (densified.length % 2 === 0) {
-    fourierX = dftEven(x); //DFT low frequencies even
-  } else {
-    fourierX = dftOdd(x); //DFT low frequencies odd
-  }
-
+  const isEven = densified.length % 2 === 0;
+  fourierX = isEven ? dftEven(x) : dftOdd(x);
   fourierX.sort((a, b) => b.amp - a.amp);
+}
 
-  // Set initial potition
-  time = 0;
-
-  //UI
+function setupUI() {
   sel = createSelect();
   sel.position(10, 10);
+  sel.style('font-size:16px');
   sel.option('Epicycles');
   sel.option('Approx. Curve');
-  sel.style('font-size:16px');
-  sel.changed(options);
+  sel.changed(handleModeChange);
 
   sliderTerms = createSlider(3, fourierX.length, 1000, 1);
   sliderTerms.style('width', '400px');
   sliderTerms.position(windowWidth / 2 - 200, windowHeight - 70);
-  sliderTerms.changed(clearPath);
-
-  console.log(fourierX.length);
-  // Maybe to change speed
-  frameRate(47);
+  sliderTerms.input(clearPath); // smoother than .changed()
 }
 
-function draw() {
+//===========================
+// Drawing Functions
+//===========================
+function drawEpicycles() {
+  strokeJoin(ROUND);
+  const v = epicycles(0, 0, fourierX, sliderTerms.value(), time);
 
-  background(255);
-  translate(width / 2 - 70, height / 2);
+  if (time <= TWO_PI + PI / 10) path.unshift(v);
 
-  if (show === true) {
+  noFill();
+  strokeWeight(5);
+  beginShape();
+  path.forEach(p => vertex(p.x, p.y));
+  endShape();
 
-    // The epicycles
-    strokeJoin(ROUND);
-
-    let v = epicycles(0, 0, 0, fourierX, sliderTerms.value(), time);
-    //I want to draw it just once
-    if (0 <= time && time <= TWO_PI + PI / 10) {
-      path.unshift(v);
-    }
-    beginShape();
-    noFill();
-    strokeWeight(5);
-    for (let i = 0; i < path.length; i++) {
-      vertex(path[i].x, path[i].y);
-    }
-    endShape();
-
-    // Message for epycicles
-    translate(-width / 2, -height / 2);
-    textAlign(CENTER);
-    textSize(17);
-    strokeWeight(1);
-    stroke(0);
-    fill(0);
-    text('' + sliderTerms.value() + ' epicycles', windowWidth / 2, 30);
-
-  } else {
-
-    // The approximation curve
-    strokeWeight(5);
-    stroke(0);
-    strokeJoin(ROUND);
-    noFill();
-    beginShape();
-    for (let k = 0; k < 360; k+=0.5) {
-      let vs = fourierSeries(fourierX, radians(k), sliderTerms.value());
-      vertex(vs.x, vs.y);
-    }
-    endShape(CLOSE);
-
-    // Message for approx curve
-    translate(-width / 2, -height / 2);
-    textAlign(CENTER);
-    textSize(17);
-    strokeWeight(1);
-    stroke(0);
-    fill(0);
-    text('Parametric curve with n=' + sliderTerms.value() + ' terms', windowWidth / 2, 30);
-
-  }
-
-  // Update time: two options
-  const dt = 0.006;
-  //const dt = TWO_PI / fourierX.length;
-  time += dt;
-
+  drawLabel(`${sliderTerms.value()} epicycles`);
 }
 
-// Extra functions
+function drawApproximation() {
+  stroke(0);
+  strokeWeight(5);
+  strokeJoin(ROUND);
+  noFill();
 
-function options() {
-  var item = sel.value();
-  if (item === 'Epicycles') {
-    clearPath();
-    time = 0;
-    show = true;
-  } else {
-    show = false;
+  beginShape();
+  for (let k = 0; k < 360; k += 0.5) {
+    const v = fourierSeries(fourierX, radians(k), sliderTerms.value());
+    vertex(v.x, v.y);
   }
+  endShape(CLOSE);
+
+  drawLabel(`Parametric curve with n = ${sliderTerms.value()} terms`);
+}
+
+function drawLabel(txt) {
+  push();
+  resetMatrix();
+  textAlign(CENTER);
+  textSize(17);
+  strokeWeight(1);
+  stroke(0);
+  fill(0);
+  text(txt, windowWidth / 2, 30);
+  pop();
+}
+
+//===========================
+// Interaction Functions
+//===========================
+function handleModeChange() {
+  showEpicycles = sel.value() === 'Epicycles';
+  clearPath();
+  time = 0;
 }
 
 function clearPath() {
@@ -151,33 +146,37 @@ function clearPath() {
   time = 0;
 }
 
-function epicycles(x, y, rotation, fourier, terms, t) {
-  // I believe I don't need 'rotation' variable
-  // I will check later :)
+//===========================
+// Core Math Visualization
+//===========================
+function epicycles(x, y, fourier, terms, t) {
   for (let i = 0; i < terms; i++) {
-    let prevx = x;
-    let prevy = y;
-    let freq = fourier[i].freq;
-    let radius = fourier[i].amp;
-    let phase = fourier[i].phase;
+    const { freq, amp: radius, phase } = fourier[i];
+    const prevx = x;
+    const prevy = y;
 
-    x += radius * cos(freq * t + phase + rotation);
-    y += radius * sin(freq * t + phase + rotation);
+    x += radius * cos(freq * t + phase);
+    y += radius * sin(freq * t + phase);
 
-
-    strokeWeight(1.5);
+    // Draw circles and arms
     stroke(0, 0, 255);
+    strokeWeight(1.5);
     noFill();
     ellipse(prevx, prevy, radius * 2);
+
     stroke(20);
     strokeWeight(2);
     line(prevx, prevy, x, y);
   }
+
   return createVector(x, y);
 }
 
+//===========================
+// Responsive
+//===========================
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  scl = 0.003 * width;
+  setupScale();
   sliderTerms.position(windowWidth / 2 - 200, windowHeight - 70);
 }
