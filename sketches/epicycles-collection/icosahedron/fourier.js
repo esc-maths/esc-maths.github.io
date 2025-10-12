2bcf40cbff107104f76b38ad3a249913701e5940
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2025-10-12 16:00:56 +1000

    Update fourier.js

--- sketches/epicycles-collection/icosahedron/fourier.js
+++ sketches/epicycles-collection/icosahedron/fourier.js
@@ -1,13 +1,17 @@
-class Complex {
+//===========================
+// Complex Number Class
+//===========================
 
-  constructor(a, b) {
-    this.re = a;
-    this.im = b;
+class Complex {
+  constructor(re, im) {
+    this.re = re;
+    this.im = im;
   }
 
   add(c) {
     this.re += c.re;
     this.im += c.im;
+    return this;
   }
 
   mult(c) {
@@ -15,89 +19,94 @@ class Complex {
     const im = this.re * c.im + this.im * c.re;
     return new Complex(re, im);
   }
-  
+
+  scale(factor) {
+    this.re /= factor;
+    this.im /= factor;
+    return this;
+  }
+
+  get amp() {
+    return Math.sqrt(this.re ** 2 + this.im ** 2);
+  }
+
+  get phase() {
+    return Math.atan2(this.im, this.re);
+  }
 }
 
-/* 
- Lower frequencies DFT for odd and even signals
- For more options check:
- https://github.com/jcponce/jcponce.github.io/blob/master/sketches/fourier/draw/fourier.js
-*/
 
+//===========================
+// Discrete Fourier Transform (DFT)
+//===========================
+
+/**
+ * Generic DFT function for both even and odd cases.
+ * @param {Complex[]} x - Input signal as array of Complex numbers.
+ * @param {boolean} isOdd - True for odd DFT, false for even DFT.
+ */
+function dft(x, isOdd = false) {
+  const X = [];
+  const N = x.length;
+  const half = isOdd ? (N - 1) / 2 : N / 2;
+
+  for (let k = -half; k <= (isOdd ? half : half - 1); k++) {
+    let sum = new Complex(0, 0);
+
+    for (let l = 0; l < N; l++) {
+      const phi = (k * 2 * Math.PI * l) / N;
+      const c = new Complex(Math.cos(phi), -Math.sin(phi));
+      sum.add(x[l].mult(c));
+    }
+
+    sum.scale(N);
+
+    const freq = k;
+    X[k + half] = {
+      re: sum.re,
+      im: sum.im,
+      freq,
+      amp: sum.amp,
+      phase: sum.phase
+    };
+  }
+  return X;
+}
+
+/**
+ * DFT for odd signals (lower frequencies)
+ */
 function dftOdd(x) {
-
-  const X = [];
-  const N = x.length;
-
-  for (let k = -(N - 1) / 2; k <= (N - 1) / 2; k++) {
-    let sum = new Complex(0, 0);
-    for (let l = 0; l < N; l++) {
-      const phi = k * 2 * PI * l / N;
-      const c = new Complex(cos(phi), -sin(phi));
-      sum.add(x[l].mult(c));
-    }
-    // Average
-    sum.re = sum.re / N;
-    sum.im = sum.im / N;
-
-    let freq = k;
-    let amp = sqrt(sum.re * sum.re + sum.im * sum.im);
-    let phase = atan2(sum.im, sum.re);
-    X[k + (N - 1) / 2] = {
-      re: sum.re,
-      im: sum.im,
-      freq,
-      amp,
-      phase
-    };
-  }
-  return X;
+  return dft(x, true);
 }
 
-//Low frequencies DFT Even
+/**
+ * DFT for even signals (lower frequencies)
+ */
 function dftEven(x) {
-
-  const X = [];
-  const N = x.length;
-
-  for (let k = -N / 2; k <= N / 2 - 1; k++) {
-    let sum = new Complex(0, 0);
-    for (let l = 0; l < N; l++) {
-      const phi = k * 2 * PI * l / N;
-      const c = new Complex(cos(phi), -sin(phi));
-      sum.add(x[l].mult(c));
-    }
-    //Average
-    sum.re = sum.re / N;
-    sum.im = sum.im / N;
-
-    let freq = k;
-    let amp = sqrt(sum.re * sum.re + sum.im * sum.im);
-    let phase = atan2(sum.im, sum.re);
-    X[k + N / 2] = {
-      re: sum.re,
-      im: sum.im,
-      freq,
-      amp,
-      phase
-    };
-  }
-  return X;
+  return dft(x, false);
 }
 
-// Fourier series representation using the fourier coefficients
+
+//===========================
+// Fourier Series Reconstruction
+//===========================
+
+/**
+ * Fourier series representation using Fourier coefficients.
+ * @param {Array} fourier - Array of Fourier coefficients.
+ * @param {number} t - Time or angle parameter.
+ * @param {number} terms - Number of terms to use in the reconstruction.
+ */
 function fourierSeries(fourier, t, terms) {
-  let c = fourier;
   let sumX = 0;
   let sumY = 0;
-  let k = 0;
-  while (k < terms) {
-    let f = c[k].freq;
-    let r = c[k].re;
-    let i = c[k].im;
-    sumX += cos(f * t) * r - sin(f * t) * i;
-    sumY += cos(f * t) * i + sin(f * t) * r;
-    k++
+
+  for (let k = 0; k < terms; k++) {
+    const { freq: f, re: r, im: i } = fourier[k];
+    sumX += Math.cos(f * t) * r - Math.sin(f * t) * i;
+    sumY += Math.cos(f * t) * i + Math.sin(f * t) * r;
   }
+
   return createVector(sumX, sumY);
 }
