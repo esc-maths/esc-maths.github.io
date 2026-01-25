1add343f81962a9e6960bcb67651b6b037f88847
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2026-01-26 07:31:20 +1000

    Update index.js

--- sketches/starts-field/index.js
+++ sketches/starts-field/index.js
@@ -1,14 +1,17 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { color, positionLocal, screenUV, range, time, vec3, mod, float, uv } from "three/tsl";
+import { color, positionLocal, screenUV, range, time, vec3, mod, float, uv, pass } from "three/tsl";
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene()
 scene.fog = new THREE.FogExp2(0x000000,0.02);
 scene.background = new THREE.Color(0x000000);
+
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 1;
+
 const renderer = new THREE.WebGPURenderer({ antialias: true });
+
 renderer.setAnimationLoop(animate);
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
@@ -17,14 +20,18 @@ const ctrls = new OrbitControls(camera, renderer.domElement);
 ctrls.enableDamping = true;
 
 const startCount = 100000;
-const size = 0.25;
+const size = 0.2;
 const geometry = new THREE.PlaneGeometry(size, size);
+
+const positionRange = range(new THREE.Vector3(-100, -100, -100), new THREE.Vector3(100, 100, 100));
+const brightnessRange = range(0.1, 1.2);
+
 const material = new THREE.MeshBasicNodeMaterial({
   //color: 0xffffff}
   transparent: true,
+  alphaTest: 0.1
 });
 
-const positionRange = range(new THREE.Vector3(-100, -100, -100), new THREE.Vector3(100, 100, 100));
 const speed = 10;
 const animatedZ = mod(positionRange.z.add(time.mul(speed)), 100.0).sub(100);
 const animatedPos = vec3(
@@ -33,7 +40,6 @@ const animatedPos = vec3(
   animatedZ
 )
 
-const brightnessRange = range(0.1, 1.2);
 const colorRange = range(color(0x000000), color(0xffffff));
 material.positionNode = positionLocal.add(animatedPos);
 material.opacityNode = float(0.1).div(uv().sub(0.5).length()).sub(0.2);
@@ -42,22 +48,13 @@ material.colorNode = colorRange.mul(brightnessRange)
 const starts = new THREE.InstancedMesh(geometry, material, startCount);
 scene.add(starts);
 
-//const knot = new THREE.Mesh(geometry, material);
-//scene.add(knot);
+// const postProcessing = new THREE.PostProcessing(renderer);
+// const scenePass = pass(scene, camera);
+// const scenePassColor = scenePass.getTextureNode();
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-scene.add(hemiLight);
+// const afterImageEffect = afterImage(scenePassColor, 0.97);
 
-// Sprites BG
-// const sprites = getLayer({
-//   hue: 0.6,
-//   numSprites: 8,
-//   opacity: 0.1,
-//   radius: 10,
-//   size: 24,
-//   z: -10.5,
-// });
-// scene.add(sprites);
+// postProcessing.outputNode = afterImageEffect;
 
 
 function animate() {
@@ -67,6 +64,7 @@ function animate() {
   //renderer.renderAsync(scene, camera);
 
   renderer.render(scene, camera);
+  //postProcessing.render();
   ctrls.update();
 }
 
