57be95dc9681295c540e0e4f906e8743c4b2e8dc
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2026-02-11 19:25:13 +1000

    ok

---
+++ sketches/sea-creatures/step-01/sketch.js
@@ -0,0 +1,33 @@
+/*
+  Sea creature Step 1
+  Juan Carlos Ponce Campuzano
+  01/Feb/2026
+*/
+
+// Step 1: Basic Grid Setup
+
+function setup() {
+  createCanvas(500, 500);
+  background(0);
+  stroke(255);
+}
+
+function draw() {
+  background(0);
+
+  for (let i = 0; i < 20000; i++) {
+
+    const x = i % 200;
+    const y = floor(i / 200);
+
+    // simple recentering
+    const k = x - 100;
+    const e = y - 50;
+
+    // gentle warp
+    const px = width / 2 + k * 2;
+    const py = height / 2 + e * 2;
+
+    point(px, py);
+  }
+}
