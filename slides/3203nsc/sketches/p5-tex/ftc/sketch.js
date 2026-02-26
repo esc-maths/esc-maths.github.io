3700d31330421677bf8cd3ee3652cc17f5b4e69c
Juan Carlos Ponce Campuzano <poncecampuzanocarlos@gmail.com>
2026-02-26 12:14:52 +1000

    ok

---
+++ slides/3203nsc/sketches/p5-tex/ftc/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(600, 150);
+  overflow('hidden');
+  background(255);
+  writeTeX();
+}
+
+function writeTeX() {
+  let equation = createTeX(
+    "{\\displaystyle \\int_a^b f(x)\\,dx=F(b)-F(a)}"
+  );
+  equation.position(23, 15);
+  equation.size(48);
+  equation.stroke(color('rgb(0,0,0)'));
+  equation.fill(color('rgb(0,0,0)'));
+  
+  equation.play("createFill", 0, 3.5);
+ 
+}
