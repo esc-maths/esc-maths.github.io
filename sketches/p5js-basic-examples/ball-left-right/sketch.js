15e1962bfa2f26a204fe32d7ba073157fb30ffa1
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2026-05-28 15:51:44 +1000

    Update sketch.js

--- sketches/p5js-basic-examples/ball-left-right/sketch.js
+++ sketches/p5js-basic-examples/ball-left-right/sketch.js
@@ -19,21 +19,15 @@ function setup() {
 
 function draw() {
   background(0);
+  
+  fill(255, 255, 0);
+  circle(x, 100, diameter);
+  
+  fill(0);
+  circle(x + direction * 10, 80, 10);
+  
   x += speed * direction;
   if (x > width - diameter / 2 || x < diameter / 2) {
     direction = -direction; // Change direction
-  }
-  if (direction == 1) {
-    // Moving to the right
-    fill(255, 255, 0);
-    circle(x, 100, diameter);
-    fill(0);
-    circle(x + 10, 80, 10);
-  } else {
-    // Moving to the left
-    fill(255, 255, 0);
-    circle(x, 100, diameter);
-    fill(0);
-    circle(x - 10, 80, 10);
   }
 }
