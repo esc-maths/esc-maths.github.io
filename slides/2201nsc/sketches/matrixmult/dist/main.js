d1dd3dcfb5be0f8d6abe05455eaecb229370775d
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2026-07-29 19:11:53 +1000

    Update main.js

--- slides/2201nsc/sketches/matrixmult/dist/main.js
+++ slides/2201nsc/sketches/matrixmult/dist/main.js
@@ -1,3 +1,4 @@
+// Built by @andrestaltz with Cycle.js
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
@@ -10,7 +11,7 @@ function view(calculatorVDom) {
             dom_1.h1(".title." + styles_1.default.title, 'Matrix Multiplication'),
             calcVNode,
             dom_1.h2(".footnote." + styles_1.default.footnote, [
-                dom_1.a({ attrs: { href: 'https://x.com/andrestaltz', target: '_blank' } }, 'Built by @andrestaltz with Cycle.js')
+                dom_1.a({ attrs: { href: 'https://x.com/andrestaltz', target: '_blank' } }, '')
             ])
         ]);
     });
